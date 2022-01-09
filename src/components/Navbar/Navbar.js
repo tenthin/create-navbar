e0c7338a53657f7fa6a96379cec572c5d37ca6e5
Tenzin <tenthinten@gmail.com>
2022-01-09 22:25:59 +0530

    add hamburger bars

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,12 +3,17 @@ import { MenuItems } from './MenuItems';
 import './Navbar.css'
 
 class Navbar extends Component{
+    state = { clicked: false }
+
+
+
     render() {
         return(
             <nav className='NavbarItems'>
                 <h1 className="navbar-logo">React<i className='fab fa-react'></i></h1>
-                <div className='menu-icon'>
-                    
+                <div className='menu-icon' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}>
+                    </i>
                 </div>
                 <ul>
                     {MenuItems.map((item, index) => {
