48f9eb845a57d8d7c3b302eb5a4d7ea706fff24c
Tenzin <tenthinten@gmail.com>
2022-01-09 22:08:23 +0530

    adding font style and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react';
-
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      hi save this
+      <Navbar/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { MenuItems } from './MenuItems';
+import './Navbar.css'
 
 class Navbar extends Component{
     render() {
         return(
             <nav className='NavbarItems'>
-                <h1 className="navbar-logo">React</h1>
+                <h1 className="navbar-logo">React<i className='fab fa-react'></i></h1>
                 <div className='menu-icon'>
                     
                 </div>
                 <ul>
                     {MenuItems.map((item, index) => {
                         return (
-                            <li><a className={item.cName}></a></li>
+                            <li key = {index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}    
+                                </a>
+                            </li>
                         )
                     })}
                     
